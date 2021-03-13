d256e110929da645163210895ea6a8c47c7952fa
Taha El Bouffi <taha.elbouffi@gmail.com>
2021-03-13 23:27:15 +0100

    esbuild wasm implementation & transpiling

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,47 @@
-import ReactDOM from 'react-dom';
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-    return <div>Hello App!!</div>
-}
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const esbuildRef = useRef<any>();
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+  const startService = async () => {
+    esbuildRef.current = await esbuild.startService({
+      wasmURL: "/esbuild.wasm",
+      worker: true,
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const submitHandler = async () => {
+    if (!esbuildRef.current) {
+      return;
+    }
+    const result = await esbuildRef.current.transform(input, {
+      loader: "jsx",
+      target: 'es2015'
+    });
+
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={submitHandler}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
