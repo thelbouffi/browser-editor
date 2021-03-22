cdbebe77313b4384265c8186e4fcc1100e9b7bae
Taha El Bouffi <taha.elbouffi@gmail.com>
2021-03-22 22:17:21 +0100

    toggle text editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,9 +1,47 @@
-import MDEditor from '@uiw/react-md-editor';
+import { useState, useEffect, useRef } from "react";
+import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
-    return <div>
-        <MDEditor.Markdown source={'# header'}/>
+  const [editing, setEditing] = useState(false);
+  const textEditorRef = useRef<HTMLDivElement | null>(null);
+
+  const listener = (event: MouseEvent) => {
+    if (
+      textEditorRef.current &&
+      event.target &&
+      textEditorRef.current.contains(event.target as Node)
+    ) {
+      return;
+    }
+    setEditing(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", listener, {
+      capture: true,
+    });
+    // or document.addEventListener("click", listener, true);
+    return () => {
+      document.removeEventListener("click", listener, {
+        capture: true,
+      });
+      // or document.removeEventListener("click", listener, true);
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={textEditorRef}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
     </div>
-}
+  );
+};
 
 export default TextEditor;
