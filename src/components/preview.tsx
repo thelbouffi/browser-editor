b7bab6d648018e5393b686dabdee6960435e10a4
Taha El Bouffi <taha.elbouffi@gmail.com>
2021-03-22 13:19:15 +0100

    adding code execution timeout

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,4 @@
-import './preview.css'
+import "./preview.css";
 import { useEffect, useRef } from "react";
 
 interface PreviewProps {
@@ -34,7 +34,9 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframeRef.current.srcdoc = html;
-    iframeRef.current.contentWindow.postMessage(code, "*");
+    setTimeout(() => {
+      iframeRef.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
 
   return (
