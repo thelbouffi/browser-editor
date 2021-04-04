f8b97effae065386235d2e6caa81a572d4346854
Taha El Bouffi <taha.elbouffi@gmail.com>
2021-04-04 19:50:01 +0100

    creating the cli

---
+++ packages/local-api/src/index.ts
@@ -0,0 +1,5 @@
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log("srvin port", port);
+  console.log("srvin filename", filename);
+  console.log("srvin dir", dir);
+};
